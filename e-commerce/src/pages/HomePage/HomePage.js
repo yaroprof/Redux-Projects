157c84f195eab93c40781009abf207518964d3e0
yaroprof <crosskyiv@gmail.com>
2023-03-06 11:28:29 +0200

    trhg

--- e-commerce/src/pages/HomePage/HomePage.js
+++ e-commerce/src/pages/HomePage/HomePage.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Slider from '../../components/Slider/Slider';
 import Category from '../../components/Category/Category';
-import ProductList from '../../components/ProductList/ProductList';
+// import ProductList from '../../components/ProductList/ProductList';
 
 import './HomePage.scss';
+import { fetchCategories } from '../../store/categorySlice';
 
 export default function HomePage() {
+
+  const dispatch = useDispatch();
+
+  const{ data: categories, status: categoryStatus } = useSelector((state) => state.Category)
+
+  useEffect(() => {
+    dispatch(fetchCategories())
+  })
+
   return (
     <div className='home-page'>
       <Slider />
-      <Category />
-      <ProductList />
+      <Category categories={categories} status={categoryStatus} />
+      {/* <ProductList /> */}
     </div>
   )
 }
